c467c663d6806df7298ef67e4911693a6711a4fd
Zimm1 <cavazzoni.simone.98@gmail.com>
2020-04-08 15:21:21 +0200

    Added new gapi credentials

--- client/common/config/youtube/api_keys.js
+++ client/common/config/youtube/api_keys.js
@@ -10,5 +10,13 @@ const YOUTUBE_API_KEYS = [
     "AIzaSyCwcEb0VX8NHo3qXGDw5HWSrITr1P8ZbCg",
     "AIzaSyD26u7tz1l4XixyfzBtBeiot1PmlAf9fjw",
     "AIzaSyAT-bW04Nq_5B3qFVljcS4L36cMY3c_Ebs",
-    "AIzaSyCZgsgGiCA1gVz9CWcVf4Ck1uq5uuMl9fw"
+    "AIzaSyCZgsgGiCA1gVz9CWcVf4Ck1uq5uuMl9fw",
+    "AIzaSyDrzoPPowbml44WF6yFfyD9TgqqXtJPTz0",
+    "AIzaSyBFo6VUxJO9Vxbl06qixbp98ANY_DjAH2o",
+    "AIzaSyCoepgsrtrPgjrQaFVMla4ecizpS7TPFrM",
+    "AIzaSyCWimUHsMQtbIWlaUeyXEgaWZbxwrc1rx8",
+    "AIzaSyCWoIJ_1Dsy9zpyVO3rmr5e4WDGiWuTod0",
+    "AIzaSyB4ELLQ5Y7qVph3FzuAtotf1Mz8x9FEOcQ",
+    "AIzaSyAv89Wk1HOx5Jg1PT5ibiioonAY2ovrWlM",
+    "AIzaSyBgk7Iy-YgS5wLbfsbE61IExg5zjhtOapQ"
 ];
